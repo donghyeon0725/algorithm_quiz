b6c382b999d6c5bec8a6b7db3b98ba8d33bdc13b
donghyeon <ehdgus5015@gmail.com>
2021-03-28 01:46:27 +0900

    add 너비 우선 탐색을 통한 최단거리 (최소회수로 정렬하기 문제) 관련 풀이 정리

--- src/BFS/one.java
+++ src/BFS/one.java
@@ -7,7 +7,7 @@ public class one {
     /**
      * 정수의 배열이 주어질 때 연속된 부분 구간의 순서를 뒤집는 것을 뒤집기 연산이라고 부릅시다.
      *
-     * 예를 들어 배열 {3,4,1,2}에서 부분 구간 {4,1,2}}를 뒤집으면 {3,2,1,4}가 됩니다.
+     * 예를 들어 배열 {3,4,1,2}에서 부분 구간 {4,1,2}를 뒤집으면 {3,2,1,4}가 됩니다.
      *
      * 중복이 없는 정수 배열을 뒤집기 연산을 이용해서 정렬하려고 합니다.
      * 필요한 최소한의 연산수를 계산하는 프로그램을 작성하세요.
@@ -17,87 +17,78 @@ public class one {
      * */
 
     public static void main(String[] args) {
-        int size = 8;
         int[] input1 ={1,2,3,4,8,7,5,6};
         int[] input2 ={1,2,3,4,8,7,6,5};
         int[] input3 ={3, 9999, 1, 2};
         int[] input4 ={1000, 2000, 3000};
-        System.out.println(searchPath(input1));
-        System.out.println(searchPath(input2));
-        System.out.println(searchPath(input3));
-        System.out.println(searchPath(input4));
 
-
+        System.out.println(searchPath(input1)); // 2
+        System.out.println(searchPath(input2)); // 1
+        System.out.println(searchPath(input3)); // 2
+        System.out.println(searchPath(input4)); // 0
     }
-
-
-
-    static int[] solve(int[] arr) {
-        // perm 을 [0, ...,n-1] 의 순열로 변환한다.
-        int size = arr.length;
-        int[] result = new int[size];
-
-        // 배열 전체 반복
-        for (int i=0; i<size; i++) {
-            int smaller = 0;
-
-            // 기준이 되는 값보다 작은 값이 많을 수록 상대값이 +1 된다.
-            for (int j=0; j<size; j++) {
-                if (arr[j] < arr[i])
-                    ++smaller;
-            }
-
-            result[i] = smaller;
-        }
-
-        return result;
-    }
-
 
     /**
-     * 배열 자체를 정점으로 보고, 뒤집기 연산으로 도착할 수 있는 지점을 다른 정점으로 바라볼 때,
+     * 배열 자체를 정점으로 보고,
+     * 뒤집기 연산으로 만들 수 있는 배열을 다른 정점으로 바라볼 때,
      * 최종으로 정렬된 정점까지의 거리를 구하면 되는 문제
      *
      * 이 때 핵심은 배열을 키 값으로 사용한다는 점이다.
      *
-     * 단, 배열을 그대로 사용할 경우, 주소 복사가 일어나서 값은 같아도 같은 키값으로 인식하지 않는다.
-     * 이때, Arrays 클래스에서 제공하는 deepHashCode 를 사용하면, 쉽게 같은 배열인지 구별할 수 있게 되는데,
+     * 배열을 그대로 사용할 경우, 주소 복사가 일어나서 배열의 값이 같아도 다른 객체로 인식할 수 있기 때문에 주의한다.
+     * 이때, Arrays 클래스에서 제공하는 deepHashCode 를 사용하면, 쉽게 같은 배열인지 구별할 수 있다.
      *
-     * int 배열을 사용할 수는 없고 래퍼클래스인 Integer[] 를 이용해야한다.
+     * 단, int 배열을 사용할 수는 없고 래퍼클래스인 Integer[] 를 이용해야한다.
      */
     static int searchPath(int[] input) {
+        // 배열을 복사해둔다. => 정렬할 배열을 별도로 만들어둔다. 이것은 비교 대상이 된다.
         int[] sorted = input.clone();
-        // Arrays.sort 는 기존의 배열을 정렬하기는 하나, 새로운 배열을 return 하는 것은 아니라는 사실을 주의한다.
+        // 기존의 배열을 정렬한다.
         Arrays.sort(sorted);
 
-        Map<Integer, Integer> discovered_distance = new HashMap<>();
+        // 배열의 해시코드를 key 값으로 사용하고, value는 해당 정점(뒤집은 배열)까지의 깊이를 의미한다.
+        Map<Integer, Integer> discovered = new HashMap<>();
+        // 방문 예정인 정점 (발견한 배열)을 저장하는 저장 공간
+        // 너비 우선 탐색을 가능하게 해주는 자료형은 FIFO 구조를 가진 Queue를 사용해야한다.
         Queue<int[]> queue = new LinkedList<>();
 
+        // 시작 시점을 집어 넣는다.
         queue.add(input);
-        discovered_distance.put(hash(input), 0);
+        discovered.put(hash(input), 0);
 
+        // 너비 우선 탐색 시작
+        // 방문할 정점이 있으면
         while (!queue.isEmpty()) {
-            // 랜덤하게 정렬해야함
-            int[] newArr = queue.poll();
+            // 방문할 정점(배열)을 꺼낸다.
+            int[] here = queue.poll();
+            // 발견한 배열의 해시코드 값
+            int key_here = hash(here);
 
-            if (Arrays.equals(newArr, sorted)) {
-                return discovered_distance.get(hash(newArr));
+            // 정렬된 배열과 동일한 배열을 찾으면 반복을 멈춘다.
+            if (Arrays.equals(here, sorted)) {
+                return discovered.get(key_here);
             }
 
             // 가능한 모든 부분을 뒤집어 본다.
-            // 두께
-            for (int i=0; i<newArr.length; i++) {
-                // 시작 부분
-                for (int t=i+2; t<=newArr.length; t++) {
-                    int[] copy = newArr.clone();
+            for (int i=0; i<here.length; i++) {
+                // 크기 2 이상부터 시작
+                for (int t=i+2; t<=here.length; t++) {
+                    // 배열을 뒤집기 전에 복사본을 만들어 놓음
+                    int[] copy = here.clone();
 
-                    // 뒤집은 copy 배열
+                    // 뒤집은 copy 배열 => 새로 만든 배열
                     reverse(copy, i, t);
+                    // 새로 발견한 배열의 키값
+                    int key_there = hash(copy);
 
-                    // 새로운 베열이 발견 되지 않았으면
-                    if (discovered_distance.get(hash(copy)) == null) {
+                    // 새로운 베열 copy가 기존에 없던 배열이면
+                    if (!discovered.containsKey(key_there)) {
+                        // 깊이
+                        int deep = discovered.get(key_here);
+                        // 발견한 배열
                         queue.add(copy);
-                        discovered_distance.put(hash(copy), discovered_distance.get(hash(newArr))+ 1 );
+                        // here 배열(copy 아님) 탐색 완료
+                        discovered.put(key_there, deep + 1 );
                     }
                 }
             }
@@ -106,6 +97,9 @@ public class one {
         return -1;
     }
 
+    /**
+     * 배열을 디버깅 하기 위해 사용합니다.
+     * */
     static void toString(int[] arr) {
         System.out.print("{");
         for (int i=0; i<arr.length; i++) {
@@ -117,10 +111,20 @@ public class one {
         System.out.println("}");
     }
 
+    /**
+     * 배열의 해시코드 값을 반환합니다.
+     * 같은 배열의 경우, 같은 해시 코드값을 반환합니다.
+     * */
     static int hash(int[] arr) {
         return Arrays.deepHashCode(Arrays.stream(arr).boxed().toArray(Integer[]::new));
     }
 
+    /**
+     * 배열의 일부분 또는 전체를 뒤집는 방법
+     *
+     * reverse(arr, 0, 3) 은
+     * 배열 arr의 0번 부터 2번(3번 앞) 까지의 배열을 뒤집습니다.
+     * */
     static void reverse(int[] arr, int s, int e) {
         int[] copy = Arrays.copyOfRange(arr, s, e);
 
--- src/BFS/two.java
+++ src/BFS/two.java
@@ -6,83 +6,77 @@ import java.util.stream.Collectors;
 public class two {
 
     // 배열을 해시화 해서 키값으로 사용,
-    static Map<Integer, Integer> toSort = new HashMap<>();
+    static Map<Integer, Integer> discovered = new HashMap<>();
 
     /**
      * 미리 계산해두기
      *
-     * 이 방법은, 정렬이 되어 있지 않은 배열을 뒤집어 가면서 정렬 된 배열을 찾아가는 과정이 아닌,
-     *
+     * 정렬이 되어 있지 않은 배열을 뒤집어 가면서 정렬 된 배열을 찾아가는 과정이 아니라
      * 미리 정렬 된 배열을 뒤집어 계산해두고
-     * 뒤집어진 배열을 키값으로 뒤집은 회수를 찾아오는 방법이다.
+     * 받은 배열을 키로 변환해서 계산된 값에서 꺼내오는 방법이다.
      *
      * ********************************* 핵심 *******************************
-     * 그리고
-     * {1,2,4,3}을 뒤집나 {10, 20, 40, 30}을 뒤집나 똑같은 회수로 뒤집어야 하기 때문에
-     * 받은 배열을 미리, 상대크기(순열)로 채워진 배열 {0,1,2,3,4}를 생성한다. (크기가 5인 경우)
+     * {1,2,4,3}을 뒤집은 것은 {10, 20, 40, 30}을 뒤집은 것과 똑같은 회수로 뒤집어야 하기 때문에
+     * 받은 배열을 상대크기(순열)로 채워진 배열 {1,2,4,3}를 사용해도 된다.
+     * 이는 {0,1,3,2} 배열과 뒤집는 회수가 동일하므로 이것으로 계산을 시작할 것이다.
+     * 
+     * 그리고 이렇게 하는 이유는 배열을 해시코드로 사용할 때, 
+     * {1,2,3}과 {10,20,30}의 해시코드 값은 서로 다르기 때문에, 동일하게 맞추기 위함이다.
      *
-     * 이 배열을 뒤집어 가면서, deep를 미리 계산 해놓는다. => 1000건의 케이스를 테스트 하는 실제 문제에서 static으로 결과를 미리 만들어 놓기 때문에 계산이 빠르다.
-     * 그리고 배열을 키 값으로 deep를 찾아온다.
+     * 이 배열을 뒤집어 가면서, deep(깊이=뒤집은 회수)을 미리 계산 해놓는다. 
+     * => 1000건의 케이스를 테스트 하는 실제 알고리즘 테스트에서 static으로 결과를 미리 만들어 놓고
+     * 키(배열) 값으로 deep(깊이)을 찾아온다.
      * **************************************************************
      * */
-    static void precalc(int n) {
+    static void preCalc(int n) {
         int[] perm = new int[n];
         for (int i=0; i<n; i++) {
             // 정렬된 배열 생성
             perm[i] = i;
         }
 
+        // 방문할 정점(배열)을 저장할 공간 => 재귀 호출이 아닌, Queue 를 사용해서 저장하는 것이 핵심
         Queue<int[]> queue = new LinkedList<>();
 
         // 큐에 탐색할 대상을 넣어둔다.
         queue.add(perm);
-        // 탐색 깊이
-        toSort.put(hash(perm), 0);
+        discovered.put(hash(perm), 0);
 
         // 탐색 시작
         while (!queue.isEmpty()) {
+            // 배열을 꺼내옴
             int[] here = queue.poll();
-
-            int cost = toSort.get(hash(here));
-
-            /*for (int i=0; i<n; i++) {
-                for (int j=i+2; j<=n; j++) {
-                    reverse(here, i, j);
-
-                    int there = hash(here);
-                    if(!toSort.containsKey(there)) {
-                        // 뒤집은 배열을 넣음
-                        toSort.put(there, cost+1);
-                        queue.add(here);
-                    }
-                    // 2번 뒤집으면, 새로운 배열을 만들 필요가 없음
-                    reverse(here, i, j);
-                }
-            }*/
+            // 배열을 뒤집기 전에 미리, 해시값으로 변환하여, discovered에 저장된 깊이를 찾아옴
+            int cost = discovered.get(hash(here));
 
             for (int i=0; i<n; i++) {
-                // 시작 부분
+                // 2 크기 이상 부터 시작
                 for (int j=2+i; j<=n; j++) {
+                    int[] copy = here.clone();
 
-                    // 뒤집은 here 배열
-                    reverse(here, i, j);
+                    // 뒤집은 copy 배열
+                    reverse(copy, i, j);
 
-                    int there = hash(here);
+                    int there = hash(copy);
                     // 새로운 베열이 발견 되지 않았으면
-                    if (!toSort.containsKey(there)) {
+                    if (!discovered.containsKey(there)) {
                         // 이 때 복사본을 넣어야함
-                        queue.add(here);
-                        toSort.put(there, cost + 1 );
+                        queue.add(copy);
+                        discovered.put(there, cost + 1);
                     }
 
-                    // 복사본을 사용할 것이 아니라면, 한번 뒤집은 결과는 다시 뒤집어 놓아야, 다음 연산에 제대로 된 결과가 나옴
-                    reverse(here, i, j);
+                    // reverse(here, i, j);
                 }
             }
 
         }
     }
 
+    /**
+     * 받은 배열 값을 상대값으로 변환된 배열을 통해,
+     *
+     * 탐색한 값을 찾아온다.
+     * */
     static Integer solve(int[] perm) {
         // perm 을 [0, ...,n-1] 의 순열로 변환한다.
         int n = perm.length;
@@ -101,13 +95,20 @@ public class two {
             fixed[i] = smaller;
         }
 
-        return toSort.get(hash(fixed));
+        return discovered.containsKey(hash(fixed)) ? discovered.get(hash(fixed)) : -1;
     }
 
+    /**
+     * 해시코드로 변환
+     * 값이 같은 배열은 같은 해시코드를 반환
+     * */
     static Integer hash(int[] arr) {
         return Arrays.deepHashCode(Arrays.stream(arr).boxed().toArray(Integer[]::new));
     }
 
+    /**
+     * 배열의 일부분 또는 전체를 뒤집는 로직
+     * */
     static void reverse(int[] arr, int s, int e) {
         int[] copy = Arrays.copyOfRange(arr, s, e);
 
@@ -117,26 +118,9 @@ public class two {
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {0,1,2,3,7,6,5,4};
-        int[] arr1 = {1,2,3,4,8,7,6,5};
-
-
-        int[] input1 ={1,2,3,4,8,7,5,6};
-        //int[] input2 ={1,2,3,4,8,7,6,5};
-        int[] input2 ={1,2,3,4,8,7,6,5};
-        int[] input3 ={3, 9999, 1, 2};
-        int[] input4 ={1000, 2000, 3000};
-
-        precalc(input2.length);
-
-        //int a1 = solve(input1);
-        int a2 = solve(input2);
-
-        //System.out.println(a1);
-        System.out.println(a2);
-    }
-
+    /**
+     * 배열을 디버깅 할 때 사용
+     * */
     static void toString(int[] arr) {
         System.out.print("{");
         for (int i=0; i<arr.length; i++) {
@@ -147,4 +131,21 @@ public class two {
         }
         System.out.println("}");
     }
+
+    public static void main(String[] args) {
+        // 두 배열을 자세히 보면 좀 다릅니다
+        int[] input1 ={1,2,3,4,8,7,5,6};
+        int[] input2 ={1,2,3,4,8,7,6,5};
+
+        // 미리 계산
+        preCalc(input2.length);
+
+        int value1 = solve(input2);
+        int value2 = solve(input1);
+
+        System.out.println(value1); // 1
+        System.out.println(value2); // 2
+    }
+
+
 }
